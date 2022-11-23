c2260dc473c4fb77803abd73e0e26bdc84ae25d9
Lougheed <lougheed.wan@ucalgary.ca>
2022-11-23 14:30:09 -0700

    created comments -lougheed

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+#Main execution file: Run system off this file:
+#To run program: type "python main.py"
+
 print("Hello World")
+
+#TODO: Generate our population:
+
+#TODO: Deletion
+
+#TODO: Mutation
+
+#TODO: Crossover 
+
+#TODO: Evaluate best solutions and repeat
