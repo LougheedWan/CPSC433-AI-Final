70cd235edb73f3966b061d0b42534c37c2d01233
Lougheed <lougheed.wan@ucalgary.ca>
2022-11-28 23:48:15 -0700

    just added a note - lougheed

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ generatePopulation.generate_pop()
 
 #Example output format: output = {"CSMA U13T3 DIV 01": "MO, 10:00", "CSMA U13T3 DIV 01 PRC 01": "TU, 10:00" .... "EVAL": 30}
 #We use a dictonary to store ONE valid schedule, our populaton will be an array of n length of these dictonaries.
+#NOTE: all data is now inputed into different data structures defined in globalVariables.py, this should help with the Evaluation of best solutions.
 #All functions below should take ONE valid schedule at a time, and we shall repeat the functions for all schedules
 
 #TODO: Deletion (Alexis)
