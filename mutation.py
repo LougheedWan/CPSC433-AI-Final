fcd9944938185e76a05895abf734c3ba06163587
Isaac966 <79025865+Isaac966@users.noreply.github.com>
2022-12-07 13:31:17 -0700

    Update mutation.py

--- mutation.py
+++ mutation.py
@@ -1,33 +1,41 @@
-# implements the operations of the mutation extension rule
-
 import generatePopulation
 import globalVariables
 import math
 import random
 
-def mutation (currentPop):
+def mutate (currentPop):
 
     population = currentPop
     popNum = len(population)
-    popMax = 1000
+    popMax = 10
     
     # mutateNum is the number of facts in the population that will be mutated
-    mutateNum = random.randrange(1, (popMax - popNum))
+    mutateNum = random.randint(1, (popMax - popNum))
 
-for i in mutateNum:
+    for i in range(mutateNum):
     
-    randomFact = random.choice(population.games + population.practices)
-    randomSlot = random.choice(list(population.gameSlots) + list(population.practiceSlots))
+        #select a random game/practice
+        randomFact = random.choice(globalVariables.games + globalVariables.practices)
+        #select a random time slot
+        randomSlot = random.choice(list(globalVariables.gameSlots) + list(globalVariables.practiceSlots))
     
-    notCompatible = False
-    maxReach = False
-    #check notCompatible
-    notCompatible = checkNotCompatible(randomFact, randomSlot)
-    #check for max 
-    maxReach = checkMax(randomSlot)
+        notCompatible = False
+        maxReach = False
+        #check notCompatible
+        notCompatible = generatePopulation.checkNotCompatible(randomFact, randomSlot)
+        #check for max 
+        if randomSlot in globalVariables.practiceSlots:
+            maxReach = generatePopulation.checkPracticeMax(randomSlot)
+        else: 
+            maxReach = generatePopulation.checkGameMax(randomSlot)
     
-    if notCompatible == False & maxReach == False:
-        population.schedule.update({randomFact: randomSlot})
-        #decrease gamemax or practicemax
-      
+        if notCompatible == False & maxReach == False:
+            globalVariables.schedule.update({randomFact: randomSlot})
+            #decrease gamemax or practicemax
+            if randomSlot in globalVariables.practiceSlots:
+                globalVariables.practiceSlots[str(randomSlot).strip()]["practicemax"] = int(globalVariables.practiceSlots[str(randomSlot).strip()]["practicemax"]) - 1
+            else:
+                globalVariables.gameSlots[str(randomSlot).strip()]["gamemax"] = int(globalVariables.gameSlots[str(randomSlot).strip()]["gamemax"]) - 1
     
+    return population
+
